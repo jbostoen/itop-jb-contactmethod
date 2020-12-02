a26913c995ca3edec52edbbfb791d782ef500305
jbostoen <->
2020-12-02 13:58:12 +0100

    * first repo

---
+++ module.jb-contactmethod.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2019 Jeffrey Bostoen
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2020-12-02 13:56:48
+ *
+ * PHP Main file
+ */
+
+SetupWebPage::AddModule(
+        __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+        'jb-contactmethod/2.6.201202',
+        array(
+                // Identification
+                //
+                'label' => 'Datamodel: Contact Method',
+                'category' => 'business',
+
+                // Setup
+                //
+                'dependencies' => array( 
+                ),
+                'mandatory' => false,
+                'visible' => true,
+
+                // Components
+                //
+                'datamodel' => array(
+					'model.jb-contactmethod.php',
+					'app/application/applicationextension.inc.php',
+					'app/common/phonehelper.class.inc.php'
+                ),
+                'webservice' => array(
+
+                ),
+                'data.struct' => array(
+                        // add your 'structure' definition XML files here,
+                ),
+                'data.sample' => array(
+                        // add your sample data XML files here,
+                ),
+
+                // Documentation
+                //
+                'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+                'doc.more_information' => '', // hyperlink to more information, if any
+
+                // Default settings
+                //
+                'settings' => array(
+                        // Module specific settings go here, if any
+                ),
+        )
+);
